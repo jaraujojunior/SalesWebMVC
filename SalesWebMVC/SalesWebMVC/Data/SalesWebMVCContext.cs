06abf230b7c50f55ddc5e6c3ac96f94032df2049
DESKTOP-S0GOU27\Araujo Junior <junior@unialltech.com.br>
2024-05-10 00:36:30 -0300

    Add Other Entities and create second migration

--- SalesWebMVC/SalesWebMVC/Data/SalesWebMVCContext.cs
+++ SalesWebMVC/SalesWebMVC/Data/SalesWebMVCContext.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using SalesWebMVC.Models;
 
 namespace SalesWebMVC.Data
@@ -14,6 +10,9 @@ namespace SalesWebMVC.Data
         {
         }
 
-        public DbSet<SalesWebMVC.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; } = default!;
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
+
     }
 }
